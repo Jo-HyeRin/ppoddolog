0dcab8ce9c2461755b6aab07d0c2b7884cd633b4
Hyerinism <sayhyelin12@naver.com>
2023-01-09 07:49:48 +0900

    중복확인 완료

--- src/main/resources/static/js/joinUsers.js
+++ src/main/resources/static/js/joinUsers.js
@@ -47,3 +47,202 @@ function setThumbnail(event) {
     };
     reader.readAsDataURL(event.target.files[0]);
 }
+
+// 아이디 중복체크
+let isUsernameSameCheck = false;
+let checkedUsername = "";
+$("#btnUsernameSameCheck").click(() => {
+    if ($("#username").val() == "") {
+        alert("아이디를 입력하세요");
+        return;
+    } else {
+        let username = $("#username").val();
+
+        $.ajax("/checkUsername/" + username, {
+            type: "GET",
+            dataType: "json",
+            async: true,
+            error: function (data, status, error) {
+                alert(data.responseText);
+            },
+        }).done((res) => {
+            if (res.code == 1) {
+                if (res.data == false) {
+                    alert(res.msg);
+                    isUsernameSameCheck = true;
+                    checkedUsername = $("#username").val();
+                } else {
+                    isUsernameSameCheck = false;
+                    $("#username").val("");
+                }
+            }
+        });
+    }
+});
+
+// 닉네임 중복체크
+let isNicknameSameCheck = false;
+let checkedNickname = "";
+$("#btnNicknameSameCheck").click(() => {
+    if ($("#nickname").val() == "") {
+        alert("닉네임을 입력하세요");
+        return;
+    } else {
+        let nickname = $("#nickname").val();
+
+        $.ajax("/checkNickname/" + nickname, {
+            type: "GET",
+            dataType: "json",
+            async: true,
+            error: function (data, status, error) {
+                alert(data.responseText);
+            },
+        }).done((res) => {
+            if (res.code == 1) {
+                if (res.data == false) {
+                    alert(res.msg);
+                    isNicknameSameCheck = true;
+                    checkedNickname = $("#nickname").val();
+                } else {
+                    isNicknameSameCheck = false;
+                    $("#nickname").val("");
+                }
+            }
+        });
+    }
+});
+
+// 이메일 중복체크
+let isEmailSameCheck = false;
+let checkedEmail = "";
+$("#btnEmailSameCheck").click(() => {
+    if ($("#email").val() == "") {
+        alert("이메일을 입력하세요");
+        return;
+    } else {
+        let email = $("#email").val();
+
+        $.ajax("/checkEmail/" + email, {
+            type: "GET",
+            dataType: "json",
+            async: true,
+            error: function (data, status, error) {
+                alert(data.responseText);
+            },
+        }).done((res) => {
+            if (res.code == 1) {
+                if (res.data == false) {
+                    alert(res.msg);
+                    isEmailSameCheck = true;
+                    checkedEmail = $("#email").val();
+                } else {
+                    isEmailSameCheck = false;
+                    $("#email").val("");
+                }
+            }
+        });
+    }
+});
+
+// 연락처 중복체크
+let isPhoneSameCheck = false;
+let checkedPhone = "";
+$("#btnPhoneSameCheck").click(() => {
+    if ($("#phone").val() == "") {
+        alert("연락처를 입력하세요");
+        return;
+    } else {
+        let phone = $("#phone").val();
+
+        $.ajax("/checkPhone/" + phone, {
+            type: "GET",
+            dataType: "json",
+            async: true,
+            error: function (data, status, error) {
+                alert(data.responseText);
+            },
+        }).done((res) => {
+            if (res.code == 1) {
+                if (res.data == false) {
+                    alert(res.msg);
+                    isPhoneSameCheck = true;
+                    checkedPhone = $("#phone").val();
+                } else {
+                    isPhoneSameCheck = false;
+                    $("#phone").val("");
+                }
+            }
+        });
+    }
+});
+
+// 회원가입
+$("#btnJoin").click(() => {
+    if (isUsernameSameCheck == false) {
+        alert("아이디 중복 체크를 진행해주세요");
+        return;
+    } else if (checkedUsername != $("#username").val()) {
+        alert("가입할 아이디가 상이합니다. 체크된 아이디 : " + checkedUsername);
+        return;
+    }
+
+    if (isNicknameSameCheck == false) {
+        alert("닉네임 중복 체크를 진행해주세요");
+        return;
+    } else if (checkedNickname != $("#nickname").val()) {
+        alert("가입할 닉네임이 상이합니다. 체크된 닉네임 : " + checkedNickname);
+        return;
+    }
+
+    if (isEmailSameCheck == false) {
+        alert("이메일 중복 체크를 진행해주세요");
+        return;
+    } else if (checkedNickname != $("#nickname").val()) {
+        alert("가입할 이메일이 상이합니다. 체크된 이메일 : " + checkedEmail);
+        return;
+    }
+
+    if (isPhoneSameCheck == false) {
+        alert("연락처 중복 체크를 진행해주세요");
+        return;
+    } else if (checkedPhone != $("#phone").val()) {
+        alert("가입할 연락처가 상이합니다. 체크된 연락처 : " + checkedPhone);
+        return;
+    }
+
+    join();
+});
+
+function join() {
+    let data = {
+        username: $("#username").val(),
+        password: $("#password").val(),
+        realname: $("#realname").val(),
+        nickname: $("#nickname").val(),
+        email: $("#email").val(),
+        address: $("#postcode").val() + "," + $("#addr").val() + "," + $("#detailAddress").val(),
+        phone: $("#phone").val(),
+    };
+    let formData = new FormData();
+    formData.append('file', $("#file")[0].files[0]);
+    formData.append('joinDto', new Blob([JSON.stringify(data)], { type: "application/json" }));
+
+    $.ajax("/join", {
+        type: "POST",
+        data: formData,
+        processData: false,
+        contentType: false,
+        enctype: 'multipart/form-data',
+        error: function (data, status, error) {
+            alert(data.responseText);
+        },
+    }).done((res) => {
+        if (res.code == 1) {
+            alert(res.msg);
+            location.href = "/loginForm";
+        } else {
+            alert(res.msg);
+            return false;
+        }
+    });
+}
